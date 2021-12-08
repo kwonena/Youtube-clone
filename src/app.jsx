217606a9e1b527e55781b1e2648715f31ce31c41
KwonEna <eeena214@gmail.com>
2021-12-08 23:33:56 +0900

    Feat : video list 구현 및 스타일 적용

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,27 @@
-import './app.css';
-import VideoList from './components/videoList';
+import React, { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
   const [videos, setVideos] = useState([]);
-  return <VideoList />
+
+  useEffect(() => {
+    // 컴포넌트가 마운트 되거나 업데이트 될 때만 호출
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow", // 리퀘스트시 옵션 전달
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDdBBgdy1VodASyxNV9-SoJ2qvpUuWqijY",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
