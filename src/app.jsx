0f6802413bb982051bf6faedc5c412f829a0f2c6
KwonEna <eeena214@gmail.com>
2021-12-13 23:51:48 +0900

    Refactor : 검색 기능 코드

--- src/app.jsx
+++ src/app.jsx
@@ -3,28 +3,23 @@ import styles from "./app.module.css";
 import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
+  };
 
   useEffect(() => {
-    // 컴포넌트가 마운트 되거나 업데이트 될 때만 호출
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow", // 리퀘스트시 옵션 전달
-    };
-
-    fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDdBBgdy1VodASyxNV9-SoJ2qvpUuWqijY",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
 
   return (
     <div className={styles.app}>
-      <SearchHeader />
+      <SearchHeader onSearch={search} />
       <VideoList videos={videos} />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app";
+import Youtube from "./components/service/youtube";
 
+const youtube = new Youtube(process.env.REACT_APP_YPUTUBE_KEY);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
