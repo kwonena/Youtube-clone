9d66e9eeb8cea926975d6f92978a87a813edf0b3
kwonena <eeena214@gmail.com>
2023-02-21 15:43:57 +0900

    [TEST] VideoCard 컴포넌트 테스트 코드 작성 # 공통 요소 tests 파일로 분리

---
+++ src/components/test/VideoCard.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes, useLocation } from "react-router-dom";
+import VideoCard from "../VideoCard";
+import { formatAgo } from "../../util/date";
+import userEvent from "@testing-library/user-event";
+import { fakeVideo as video } from "../../tests/videos";
+import { withRouter } from "../../tests/utils";
+
+describe("VideoCard", () => {
+  const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
+  it("renders video item", () => {
+    render(
+      withRouter(<Route path="/" element={<VideoCard video={video} />} />)
+    );
+
+    const image = screen.getByRole("img");
+    expect(image.src).toBe(thumbnails.medium.url);
+    expect(image.alt).toBe(title);
+
+    expect(screen.getByText(title)).toBeInTheDocument();
+    expect(screen.getByText(channelTitle)).toBeInTheDocument();
+    expect(screen.getByText(formatAgo(publishedAt))).toBeInTheDocument();
+  });
+
+  it("navigates to detailed video", async () => {
+    function LocationStateDisplay() {
+      return <pre>{JSON.stringify(useLocation().state)}</pre>;
+    }
+    render(
+      withRouter(
+        <>
+          <Route path="/" element={<VideoCard video={video} />} />
+          <Route
+            path={`/videos/watch/${video.id}`}
+            element={<LocationStateDisplay />}
+          />
+        </>
+      )
+    );
+
+    const card = screen.getByRole("listitem");
+    await userEvent.click(card);
+
+    expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
+  });
+});
