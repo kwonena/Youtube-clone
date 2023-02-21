6ddfae0b87c1b4c2e9db6dc8bb7450793397d983
kwonena <eeena214@gmail.com>
2023-02-21 16:34:58 +0900

    [TEST] VideoCard 정적 클래스 snapshot 코드 작성

--- src/components/test/VideoCard.test.js
+++ src/components/test/VideoCard.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter, Route, Routes, useLocation } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
+import renderer from "react-test-renderer";
 import VideoCard from "../VideoCard";
 import { formatAgo } from "../../util/date";
 import userEvent from "@testing-library/user-event";
@@ -8,6 +9,25 @@ import { withRouter } from "../../tests/utils";
 
 describe("VideoCard", () => {
   const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
+
+  it("renders grid type", () => {
+    const component = renderer.create(
+      withRouter(<Route path="/" element={<VideoCard video={video} />} />)
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it("renders list type", () => {
+    const component = renderer.create(
+      withRouter(
+        <Route path="/" element={<VideoCard video={video} type="list" />} />
+      )
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
   it("renders video item", () => {
     render(
       withRouter(<Route path="/" element={<VideoCard video={video} />} />)
